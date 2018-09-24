873d4fc69dd079d4acc7af5289ffaf8c311d7883
HamayonH <hamayon.hussain@gmail.com>
2018-09-23 20:23:09 -0500

    test 1

--- gify.js
+++ gify.js
@@ -1,12 +1,36 @@
+function Searchgiffs() {
+    var Searchbar = document.getElementById("Searchbar").value;
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + Searchbar + "&api_key=E0rUWkyrlNoHl5oyzGQoWq4ksFpAj0pY&limit=10";
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (responce) { 
+        var results = responce.data;
 
+        for (var i = 0; i < results.length; i++) {
+            var animalDiv = $("<div class=\"animal-item\">");
 
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=E0rUWkyrlNoHl5oyzGQoWq4ksFpAj0pY";
+            var rating = results[i].rating;
 
+            var p = $("<p>").text("Rating: " + rating);
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(res) {
-    console.log(res);
-  });
+            var animated = results[i].images.fixed_height.url;
+            var still = results[i].images.fixed_height_still.url;
+
+            var animalImage = $("<img>");
+            animalImage.attr("src", still);
+            animalImage.attr("data-still", still);
+            animalImage.attr("data-animate", animated);
+            animalImage.attr("data-state", "still");
+            animalImage.addClass("animal-image");
+
+            animalDiv.append(p);
+            animalDiv.append(animalImage);
+
+            $("#animals").append(animalDiv);
+        }
+    });
+
+    console.log(document.getElementById("Searchbar").value);
+}
