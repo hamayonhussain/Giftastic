f7811fb0d92a9a1d0177987093871c6297a87645
Hamayon Hussain <hamayonhussain@Hamayons-MacBook-Pro.local>
2019-02-22 23:46:24 -0600

    Updated readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Giftastic
 
-Live Link https://hamayonhussain.github.io/Giftastic/
+[Click here for a Live Demo](https://hamayonhussain.github.io/Giftastic/)
+
+This is a fun Web APP created for your entertainment using the Giphy API. You can click on buttons to see a result of Giphy that will be in still state and when you click on any of the images it will than play as giphy format.
+
+![Alt Text](https://media.giphy.com/media/YJBNjrvG5Ctmo/giphy.gif)
+
+
+### Technology
+1. HTML
+2. CSS
+3. JAVASCRIPT
+4. BOOTSTRAP
+5. JQUERY
+
+
+![Alt Text](https://media.giphy.com/media/IcGkqdUmYLFGE/giphy.gif)
--- gify.js
+++ gify.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var preRenderedGiffs = ["batman", "titanic", "tom hanks", "snakes"];
     renderButtons();
+
     function renderButtons() {
 
         // Deleting the movies prior to adding new movies
@@ -10,17 +11,17 @@ $(document).ready(function () {
         // Looping through the array of movies
         for (var i = 0; i < preRenderedGiffs.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of movie-btn to our button
-          a.addClass("search-button");
-          // Adding a data-attribute
-          a.attr("data-name", preRenderedGiffs[i]);
-          // Providing the initial button text
-          a.text(preRenderedGiffs[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
+            // Then dynamicaly generating buttons for each movie in the array
+            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+            var a = $("<button>");
+            // Adding a class of movie-btn to our button
+            a.addClass("search-button");
+            // Adding a data-attribute
+            a.attr("data-name", preRenderedGiffs[i]);
+            // Providing the initial button text
+            a.text(preRenderedGiffs[i]);
+            // Adding the button to the buttons-view div
+            $("#buttons-view").append(a);
         }
     }
 
@@ -69,9 +70,7 @@ $(document).ready(function () {
             }
         });
 
-      renderButtons();
-
-
+        renderButtons();
     }
 
     $(document).on("click", ".animal-image", function () {
@@ -92,11 +91,4 @@ $(document).ready(function () {
         console.log('search button clicked');
         Searchgiffs(this);
     });
-
-
 });
-
-
-// 1. look up the refrigetor Gamepad. 
-//  look up how to assign the onclick function to all the letters
-//  change the searchgiphy button to create create button and create a new button
